b8c2a3ab1b8822b1402cf16bf506039b4558dbaf
lucasberg29 <lucasberg29@gmail.com>
2024-09-10 23:07:04 -0400

    added death animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI.Extensions.FantasyRPG;
 
 
 public class Enemy : MonoBehaviour
@@ -29,6 +30,8 @@ public class Enemy : MonoBehaviour
     public GameObject newEnemyGroup;
 
     public Animator animator;
+
+    public ParticleSystem particleSystem;
 
     // Start is called before the first frame update
     void Start()
@@ -81,20 +84,30 @@ public class Enemy : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collider)
     {
-        if (!isDead)
+        if (isDead)
+        {
+            return;
+        }
+
+        if (hitsound != null)
         {
             hitsound.Play();
+
         }
 
 
         string gameObjectTag = collider.tag;
-
         switch(gameObjectTag)
         {
             case "Player":
                 hitsound = GameObject.Find("Player").GetComponent<AudioSource>();
 
-                hitsound.Play();
+                if (hitsound != null)
+                {
+                    hitsound.Play();
+                }
+
+
                 tank.position = GameObject.Find("Player").GetComponent<Tank>().getStartingPosition();
 
                 score.ResetScore();
@@ -102,9 +115,10 @@ public class Enemy : MonoBehaviour
 
             case "CannonBall":
                 score.addToScore();
-                //KillEnemy();
                 animator.SetBool("IsDead", true);
                 isDead = true;
+                particleSystem.Play();
+                Destroy(collider.gameObject);
                 break;
         }
     }
